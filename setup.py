9a2d00f2744e752c01a6df105fbcedca0f4fecc2
calvin <calvinchaochao@outlook.com>
2022-07-15 18:02:51 +0800

    ...

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
 from setuptools import setup
-setup(
-    name='tinycloud',
-    version='0.1',
-    description='Tinycloud',
-    author='Linyuxuan',
-    author_email='calvinchaochao@outlook.com',
-    packages=['src'],
-    install_requires=['bar', 'greek'],
-)
+setup()
